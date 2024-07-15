3d903c9ab3c9890eec0a0ca04bc2a290e889dad7
pedrosiufc <pedrosiufc@gmail.com>
2024-07-15 14:05:32 -0300

    adiconado comentários ao código Java Script da função contCamelCase.js

--- countCamelCase.js
+++ countCamelCase.js
@@ -1,9 +1,13 @@
-function actionButton() {
+/*Função que recebe uma string CamelCase arbitraria dentro da própria função e conta quantas palalvras existem */
 
+function actionButton() {
+  //string arbitraria
   var s = "myFavoriteCamelCase"
+  //contagem das inicias Maiúsculas dentro da string
   var res = s.replace(/[A-Z]/g, "");
+  //comparação do tamanho da string menos o número de letras em maiúsculas acrescentando +1 já que a primeira palavra começa em letra minúscula.
   var number = (s.length - res.length) + 1
-  //return numero;
+
 
   document.getElementById("demonstration").innerText = "Number of Words contained in the String: " + number;
 
